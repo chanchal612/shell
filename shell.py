6317f17348fb0ee0a67ea73af388ae37940979c0
chanchal612 <chanchalchauhan612@gmail.com>
2026-07-04 19:33:15 +0530

    updated commit

--- shell.py
+++ shell.py
@@ -1,6 +1,22 @@
 import sys
 import os
 
+def path_command():
+    paths = os.getenv("PATH", "").split(os.pathsep)
+
+    for path in paths:
+        full_path = os.path.join(path, command)
+
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+        
+        if os.path.isfile(full_path + ".exe") and not os.access(full_path + ".exe", os.X_OK):
+            return full_path + ".exe"
+        
+        return None 
+
+
+
 def main():
     
     while True:
@@ -16,12 +32,22 @@ def main():
                 print(f"{parts[1]} is a shell builtin")
                 continue
             else:
-                print(f"{" ".join(parts[1:])}: not found")
+
+                path = path_command(parts[1])
+
+                if path:
+                    print(f"{parts[1]} is {path}")
+                else:
+                    print(f"{" ".join(parts[1:])}: not found")
                
         elif parts[0] == "echo":
             print(" ".join(parts[1:]))
+
+
         else:
             print(f"{command}: command not found")
+
+            
 
 
 if __name__ == "__main__":
